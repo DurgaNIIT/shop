dad629672a659003d34c318f97ea85c3f2fc4209
DurgaNIIT <durgalaksh31@gmail.com>
2019-01-05 10:41:54 +0530

    Hii

---
+++ onlineshopping/src/main/webapp/WEB-INF/views/contact.jsp
@@ -0,0 +1,7 @@
+<div class="container">
+	<div class="row">
+		<h1>Contact US</h1>
+		<hr/>
+		<p>This is Contact us page</p>
+	</div>
+</div>
---
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,19 @@
+$(function(){
+	//solving active main problem
+	switch(menu){
+	
+	case 'About Us':
+		$('#about').addclass('active');
+		break;
+	case 'Contact Us':
+		$('#contact').addclass('active');
+		break;
+	default:
+		$('#home').addclass('active');
+		break;
+	
+	}
+	
+	
+	
+});
